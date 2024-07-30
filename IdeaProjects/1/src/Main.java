94e9879a0a4e593bbdc5a3571b8922d3103f864d
Persius916 <134077473+Persius916@users.noreply.github.com>
2024-07-30 10:56:02 +0530

    Delete IdeaProjects directory

--- IdeaProjects/1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("23DIT023 - ANSH KANSARA");
-        int age=25;
-        System.out.println(age+" is the age of Sam.");
-    }
-}
